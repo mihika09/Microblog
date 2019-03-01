aa23dac319a7f9cdd03ac7be6ab29a48c4337062
mihika09 <mohta.mihika@gmail.com>
2019-03-01 13:33:10 +0530

    Made explore page visible to all

--- app/routes.py
+++ app/routes.py
@@ -162,7 +162,6 @@ def followers(username):
 
 
 @app.route('/explore')
-@login_required
 def explore():
 	page = request.args.get('page', 1, type=int)
 	posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
